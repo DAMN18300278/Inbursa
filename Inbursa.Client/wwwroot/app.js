d70bb51a5d6fe96e3c060f5a1f49f0773dc95aff
Pambipapu <d.i.e.g.o.alejandro@hotmail.com>
2023-04-19 21:13:40 -0600

    Login frontend

---
+++ Inbursa.Client/wwwroot/app.js
@@ -0,0 +1,35 @@
+function iniciarFuncion(){
+    setTimeout(function(){
+        anime({
+          targets: '#icon',
+          strokeDashoffset: [anime.setDashoffset, 0],
+          duration: 2000,
+          easing: 'easeInOutSine',
+          direction: 'alternate',
+          loop: true
+        });
+      }, 200);
+    
+    document.getElementById('app').style.display = 'block';
+    animateCSS('app', 'fadeInRight'); 
+}
+
+setTimeout(iniciarFuncion, 700);
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+// We create a Promise and return it
+new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.getElementById(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+        event.stopPropagation();
+        node.classList.remove(`${prefix}animated`, animationName);
+        resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+});
