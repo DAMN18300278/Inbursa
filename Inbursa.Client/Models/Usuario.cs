6a820fe1bc793f537d5a7f66635ec2211e52ac75
LuisACarrillo <119270746+LuisACarrillo@users.noreply.github.com>
2023-04-22 01:10:50 -0600

    Login Validacion tipo de usuario

--- Inbursa.Client/Models/Usuario.cs
+++ Inbursa.Client/Models/Usuario.cs
@@ -12,4 +12,5 @@ public class Usuario
     public String Contrasena {get; set;}
     public int Autorizada {get; set;}
     public String Nom_Usuario {get; set;}
+    public String TipoDeUsuario {get; set;}
 }
